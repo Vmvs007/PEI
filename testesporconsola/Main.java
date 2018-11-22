dcbca2ddbcd91d422f79c5b25ea0c9a1663ada98
Abílio <abilio_mg_castro@hotmail.com>
2018-11-22 17:50:03 +0000

    currencyrate

--- testesporconsola/Main.java
+++ testesporconsola/Main.java
@@ -2,8 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        XMLManagement meuGestor = new XMLManagement("D:/PEI/PEI/customer.xml","D:/PEI/PEI/customerXSD.xsd");
+        XMLManagement meuGestor = new XMLManagement("D:/PEI/PEI/currencyRate.xml","D:/PEI/PEI/currencyRateXSD.xsd");
         meuGestor.read(true);
-        meuGestor.validate(true);
+        System.out.print(meuGestor.validate(true));
     }
 }

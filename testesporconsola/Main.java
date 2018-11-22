4ede88ccc4a3f5a8e0b2ab699917c831686a9419
Abílio <abilio_mg_castro@hotmail.com>
2018-11-22 18:34:09 +0000

    product feito

--- testesporconsola/Main.java
+++ testesporconsola/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        XMLManagement meuGestor = new XMLManagement("D:/PEI/PEI/currencyRate.xml","D:/PEI/PEI/currencyRateXSD.xsd");
+        XMLManagement meuGestor = new XMLManagement("D:/PEI/PEI/product.xml","D:/PEI/PEI/productXSD.xsd");
         meuGestor.read(true);
         System.out.print(meuGestor.validate(true));
     }

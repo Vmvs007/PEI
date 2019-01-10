ed2eaba2f2b98237a82d7d68d94ac9a4ce6a2156
abiliomg <abilio_mg_castro@hotmail.com>
2019-01-10 14:47:03 +0000

    Update SalesDetailsController.java

--- Parte 2/PEIProject/src/main/java/PEI/PEIProject/SalesDetailsController.java
+++ Parte 2/PEIProject/src/main/java/PEI/PEIProject/SalesDetailsController.java
@@ -15,10 +15,24 @@ public class SalesDetailsController {
     }
 
     @RequestMapping("/aggregateSalesDetailsByQueryString")
-    public String aggregateSalesDetailsByQueryString(@RequestParam(value="query") String query) {
+    public String aggregateSalesDetailsByQueryString() {
         //Example: %7B%24group%3A%7B%22_id%22%3Anull%2Ccount%3A%7B%24sum%3A1%7D%7D%7D para {$group:{"_id":null,count:{$sum:1}}} -> utilizar: https://meyerweb.com/eric/tools/dencoder/
         MongoConnector mongo = new MongoConnector();
+        String query="{$group:{_id:\"$Customer\"}},{$group:{_id:1,count:{$sum:1}}}";
         String res = mongo.aggregateDataByQueryString("PEIProject", "SalesDetails", query);
         return res;
+    }
+    @RequestMapping("/getdata")
+    public String getData(@RequestParam(value="store") String store,@RequestParam(value="mes") int mes,@RequestParam(value="ano")int ano){
+        String out="";
+        MongoConnector mongo = new MongoConnector();
+        String data=""+ano+"-0"+mes+"";
+        String query="{$match:{StoreName: {$eq: '"+store+"'},OrderDate:{$gte:'"+data+"-01 00:00:00.000',$lte:'"+data+"-31 00:00:00.000'}}}";
+
+
+
+        out=mongo.aggregateDataByQueryString("PEIProject", "SalesDetails", query);
+        return query+" - "+out;
+
     }
 }
